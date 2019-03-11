91c241e00fda210266a6b7792f2d40d0db256bd1
yasuda0102 <webnuac@gmail.com>
2019-03-12 00:16:26 +0900

    ウィンドウを赤で塗りつぶすように変更。

--- OpenGL/main.c
+++ OpenGL/main.c
@@ -1,17 +1,38 @@
+#include <GL/glew.h>
 #include <GL/glut.h>
 
 static void display(void);
 
-
 int main(int argc, char *argv[]) {
+	GLenum err;
+
+	// GLUTの初期化
 	glutInit(&argc, argv);
+
+	// ウィンドウの作成
 	glutCreateWindow(argv[0]);
+
+	// glewの初期化
+	err = glewInit();
+	if (err != GLEW_OK) {
+		return -1;
+	}
+
+	// 描画関数の設定
 	glutDisplayFunc(display);
+
+	// メインループ
 	glutMainLoop();
 
 	return 0;
 }
 
 static void display(void) {
+	GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+
+	// ウィンドウを赤で塗りつぶす
+	glClearBufferfv(GL_COLOR, 0, red);
+	glFlush();
+
 	return;
 }
